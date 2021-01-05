443351493eb829082316045cc73b58deb1d42758
Navodya Dulmith <36164924+NavodyaDulmith@users.noreply.github.com>
2021-01-06 04:15:58 +0530

    Add search

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 1/10/2019
- * Time: 3:15 PM
- */
+
 include 'header.php'; ?>
 
+<!-- search -->
+<div style="border: 1px outset #1128a4; background-color: #1128a4; ">
+    <ul data-role="listview" data-filter="true" data-filter-reveal="true" data-filter-placeholder="Search..." data-inset="true"></ul>
+</div>
+<!-- /search -->
 
 <!-- fixednavbarbottom -->
-<div data-role="footer" data-position="fixed">     
-        <div data-role="navbar" >         
-          <ul>               
-            <li><a href="#" data-icon="home" class="ui-btn-active">Home</a></li>          
-            <li><a href="#" data-icon="star">Favs</a></li>              
+<div data-role="footer" data-position="fixed">
+    <div data-role="navbar">
+        <ul>
+            <li><a href="#" data-icon="home" class="ui-btn-active">Home</a></li>
+            <li><a href="#" data-icon="star">Favs</a></li>
             <li><a href="#" data-icon="gear">Setup</a></li>
-            <li><a href="#" data-icon="gear">User</a></li>              
-          </ul>          
-        </div>
-      </div>
-      <!-- /fixednavbarbottom -->
+            <li><a href="#" data-icon="gear">User</a></li>
+        </ul>
+    </div>
+</div>
+<!-- /fixednavbarbottom -->
 
 <script>
-    jQuery(document).ready(function ($) {
+    jQuery(document).ready(function($) {
 
-        setInterval(function () {
+        setInterval(function() {
             moveRight();
         }, 5000);
 
@@ -33,16 +33,22 @@ include 'header.php'; ?>
         var slideHeight = $('#slider ul li').height();
         var sliderUlWidth = slideCount * slideWidth;
 
-        $('#slider').css({width: slideWidth, height: slideHeight});
+        $('#slider').css({
+            width: slideWidth,
+            height: slideHeight
+        });
 
-        $('#slider ul').css({width: sliderUlWidth, marginLeft: -slideWidth});
+        $('#slider ul').css({
+            width: sliderUlWidth,
+            marginLeft: -slideWidth
+        });
 
         $('#slider ul li:last-child').prependTo('#slider ul');
 
         function moveLeft() {
             $('#slider ul').animate({
                 left: +slideWidth
-            }, 200, function () {
+            }, 200, function() {
                 $('#slider ul li:last-child').prependTo('#slider ul');
                 $('#slider ul').css('left', '');
             });
@@ -51,52 +57,50 @@ include 'header.php'; ?>
         function moveRight() {
             $('#slider ul').animate({
                 left: -slideWidth
-            }, 200, function () {
+            }, 200, function() {
                 $('#slider ul li:first-child').appendTo('#slider ul');
                 $('#slider ul').css('left', '');
             });
         };
 
-        $('a.control_prev').click(function () {
+        $('a.control_prev').click(function() {
             moveLeft();
         });
 
-        $('a.control_next').click(function () {
+        $('a.control_next').click(function() {
             moveRight();
         });
 
     });
-
 </script>
 
 <script>
-   $(function () {
-    
-    /* SET PARAMETERS */
-    var change_img_time 	= 5000;	
-    var transition_speed	= 300;
-    
-    var simple_slideshow	= $("#exampleSlider"),
-        listItems 			= simple_slideshow.children('li'),
-        listLen				= listItems.length,
-        i 					= 0,
-		
-        changeList = function () {
-		
-			listItems.eq(i).fadeOut(transition_speed, function () {
-				i += 1;
-				if (i === listLen) {
-					i = 0;
-				}
-				listItems.eq(i).fadeIn(transition_speed);
-			});
+    $(function() {
 
-        };
-		
-    listItems.not(':first').hide();
-    setInterval(changeList, change_img_time);
-	
-});
+        /* SET PARAMETERS */
+        var change_img_time = 5000;
+        var transition_speed = 300;
+
+        var simple_slideshow = $("#exampleSlider"),
+            listItems = simple_slideshow.children('li'),
+            listLen = listItems.length,
+            i = 0,
+
+            changeList = function() {
+
+                listItems.eq(i).fadeOut(transition_speed, function() {
+                    i += 1;
+                    if (i === listLen) {
+                        i = 0;
+                    }
+                    listItems.eq(i).fadeIn(transition_speed);
+                });
+
+            };
+
+        listItems.not(':first').hide();
+        setInterval(changeList, change_img_time);
+
+    });
 </script>
 <?php include 'footer.php' ?>
-
