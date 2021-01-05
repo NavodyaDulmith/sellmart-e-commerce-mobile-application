189f6cfd2dacd488b80c144e1687120da110f83d
Navodya Dulmith <36164924+NavodyaDulmith@users.noreply.github.com>
2021-01-06 03:37:40 +0530

    Add menu

--- header.php
+++ header.php
@@ -25,6 +25,10 @@
             width: 50%;
             display: block;
         }
+        #white-list {
+            background: white;
+
+        }
     </style>
 
 </head>
@@ -32,10 +36,30 @@
 <body>
 
     <header data-role="header" data-position="fixed">
-
         <div data-role="header">
             <a href="#panelArea" data-iconpos="notext" data-icon="bars">menu</a>
-            <img src="./images/logo.png" style="    margin-left: auto;margin-right: auto; width: 50; display: block;" />
+            <img src="./images/logo.png" style="margin-left: auto;margin-right: auto; width: 50; display: block;" />
             <a href="profile.php" data-iconpos="notext" data-icon="user">user</a>
         </div>
     </header>
+
+    <!--menu panel -->
+    <div data-role="panel" data-position-fixed="true" data-display="overlay" data-theme="b" id="panelArea">
+        <ul id="white-list" data-role="listview">
+            <img src="images/logo.png" style="margin-left: auto;margin-right: auto; width: 80; display: block;" href="index.html" />
+            <br />
+            <h3 style="align-self: auto;">Jhon Doe</h3>
+            <br />
+            <li id="white-list"><a href="#panel-fixed-page2">Home</a></li>
+            <li><a href="#panel-fixed-page2">Categories</a></li>
+            <li><a href="#panel-fixed-page2">SellMart Points</a></li>
+            <li><a href="#panel-fixed-page2">My Orders</a></li>
+            <li><a href="#panel-fixed-page2">Favourites</a></li>
+            <li><a href="#panel-fixed-page2">Settings</a></li>
+            <li><a href="#panel-fixed-page2">Help</a></li>
+            <li><a href="#panel-fixed-page2">About Us</a></li>
+            <br />
+            <li><a href="#panel-fixed-page2">Log Out</a></li>
+        </ul>
+    </div>
+    <!-- /menu panel -->
