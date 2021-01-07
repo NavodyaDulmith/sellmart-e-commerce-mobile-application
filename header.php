9a20cc61a83291edc72d9a192dda1bdc9d48daa5
Navodya Dulmith <36164924+NavodyaDulmith@users.noreply.github.com>
2021-01-08 03:41:49 +0530

    Remove unused imports

--- header.php
+++ header.php
@@ -10,12 +10,8 @@
     <link rel="stylesheet" href="css/jquery-steps.css">
     <link href="https://fonts.googleapis.com/css?family=Scope+One" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/styles.css" />
     <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    
-
     <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
     <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 
@@ -27,6 +23,7 @@
             width: 50%;
             display: block;
         }
+
         #white-list {
             background: white;
 
@@ -38,11 +35,9 @@
 <body>
 
     <header data-role="header" data-position="fixed">
-        <div data-role="header">
-            <a href="#panelArea" data-iconpos="notext" data-icon="bars">menu</a>
-            <img src="./images/logo.png" style="margin-left: auto;margin-right: auto; width: 50; display: block;" />
-            <a href="profile.php" data-iconpos="notext" data-icon="user">user</a>
-        </div>
+        <a href="#panelArea" data-iconpos="notext" data-icon="bars">menu</a>
+        <img src="./images/logo.png" style="margin-left: auto;margin-right: auto; width: 50; display: block;" />
+        <a href="profile.php" data-iconpos="notext" data-icon="user">user</a>
     </header>
 
     <!--menu panel -->
