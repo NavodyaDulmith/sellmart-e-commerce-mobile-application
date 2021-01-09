ea73f25a4b0c37631829257f794ac096de7bfc66
Uvindu <uvindusri1996@gmail.com>
2021-01-10 02:28:04 +0530

    About Us page final

--- aboutus.php
+++ aboutus.php
@@ -9,19 +9,96 @@
             height: 300px;
             width: 100%;
         }
-    </style>
+
+        .container {
+  position: relative;
+  text-align: center;
+  color: white;
+}
+
+.bottom-left {
+  position: absolute;
+  bottom: 8px;
+  left: 16px;
+}
+
+.top-left {
+  position: absolute;
+  top: 8px;
+  left: 16px;
+}
+
+.top-right {
+  position: absolute;
+  top: 8px;
+  right: 16px;
+}
+
+.bottom-right {
+  position: absolute;
+  bottom: 8px;
+  right: 16px;
+}
+
+.centered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+img {
+  opacity: 0.5;
+}
+
+h1 {
+  color: black;
+  font-family: "Lucida Console";
+  font-size: 80px;
+}
+
+h2 {
+  padding-top: 20px;
+  color: #1128A4;
+  font-family: "Lucida Console";
+  font-size: 50px; 
+}
+</style>
+</head>
 <body>
 
-<dev div style="display:flex; justify-content:center; padding-left: 50px; padding-top:100px; align-items:center;">
-<h1>
-   About Us
-</h1>
+<div class="container">
+  <img src="images/shoppingback.jpg" alt="Snow" style="width:100%; height:400px">
+  <div class="top-left">
+  <h1>About Us</h1>
+  </div>
+</div>
+
+
+  <div style="display:flex; justify-content:left; padding-left: 50px; padding-top:20px; align-items:center;">
+  <h2>What We Do...</h2>
+    </div>
+    <div style="display:flex; justify-content:center; padding-left: 50px; padding-top:40px; align-items:center;">
+    <p style="font-family: Lucida Console;   font-size: 30px; ">We are a online marketplace for grocery<br>
+    and retail item shopping. We will deliver<br> 
+    the products for the best price and the<br> 
+    best quality in market....
+   </p>
+    </div>
+
+    <div style="display:flex; justify-content:center; padding-left: 50px; padding-top:40px; align-items:center;">
+    <h2 style="color: #1128A4;">Best Price...Best Quality</h2>
+    </div>
+    <dev div style="display:flex; justify-content:center; padding-top:40px; align-items:center;">
+    <video width="100%" controls>
+        <source src="video/shopvideo.mp4" type="video/mp4">
+        Your browser does not support HTML video.
+    </video>
 </dev>
-
-
+<div style="display:flex; justify-content:center; padding-left: 50px; padding-top:20px; padding-bottom:30px; align-items:center;">
+  <h2 style="color: #1128A4;">Where you can find us..</h2>
+    </div>
 <div id = "map">
-
-
 <script>
     function initMap() {
         var location = {lat: 6.927079, lng: 79.861244};
@@ -38,13 +115,6 @@
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB7NqG1xOvLYalZhzkl_dybcjWyKQYyg_8&callback=initMap" type="text/javascript"></script>
 </div>
 
-<dev div style="display:flex; justify-content:center; padding-left: 50px; padding-top:100px; align-items:center;">
-<video width="400" controls>
-  <source src="video/shopvideo.mp4" type="video/mp4">
-  Your browser does not support HTML video.
-</video>
-</dev>
-
-
 </body>
-</head>
+</html>
+<?php include 'footer.php' ?>
