b1d1375c41e449408f63a38601a72a7b3d608657
himashapeiris <47137407+himashapeiris@users.noreply.github.com>
2021-01-18 01:43:42 +0530

    alignment changes in splash screens

--- splashScreen.php
+++ splashScreen.php
@@ -23,26 +23,26 @@
   <body>
     <div data-role="page" id="pageOne">
       <div>
-        <img class="splashScreen1" src="images/11first.png" />
+        <img class="splashScreen1" style = "height:450; padding-bottom:0" src="images/11first.png" />
       </div>
       <div>
         <label
           class="label1"
-          style="font-weight: bold; font-size: 20px; line-height: 20px"
+          style="font-weight: bold; font-size: 23px; line-height: 20px"
           >EFFORTLESS</label
         >
         <label
           class="label2"
-          style="font-weight: bold; font-size: 20px; line-height: 20px"
+          style="font-weight: bold; font-size: 23px; line-height: 20px"
           >PURCHASE</label
         >
-        <p class="label3">
+        <p class="label3" style="font-size: 18px">
           Buy any product from any place,at any time very easily with
-          <span class="name">Sell Mart</span>
+          <span class="name" style="font-size: 18px">Sell Mart</span>
         </p>
       </div>
-      <div class="row">
-        <div class="column">
+      <div class="row" style="margin-top:6%">
+        <div class="column" >
           <button class="skipBt">
             <a href="index.html" style="color: black">Skip</a>
           </button>
@@ -58,22 +58,22 @@
     <!-- 2 nd splashScreen -->
     <div data-role="page" id="pageTwo">
       <div>
-        <img class="splashScreen2" src="images/33.jpg" />
+        <img class="splashScreen2" style = "height:400; padding-bottom:0" src="images/33.jpg" />
       </div>
       <br /><br />
       <div>
         <label
           class="label1"
-          style="font-weight: bold; font-size: 20px; line-height: 20px"
+          style="font-weight: bold; font-size: 23px; line-height: 20px"
           >EXCLUSIVE</label
         >
         <label
           class="label2"
-          style="font-weight: bold; font-size: 20px; line-height: 20px"
+          style="font-weight: bold; font-size: 23px; line-height: 20px"
           >PRODUCTS</label
         >
-        <p class="label3">
-          <span class="name">Sell Mart</span>
+        <p class="label3" style="font-size: 18px">
+          <span class="name" style = "font-size:18px">Sell Mart</span>
           with trendy products for the highest customer service
         </p>
       </div>
@@ -92,29 +92,29 @@
     <!-- 3rd splash screen -->
     <div data-role="page" id="pageThree" style="background-color: white">
       <div>
-        <img class="splashScreen2" src="images/second.jpg" />
+        <img class="splashScreen2" style = "height:400; padding-bottom:0" src="images/second.jpg" />
       </div>
       <br />
       <br />
       <div>
         <label
           class="label1"
-          style="font-weight: bold; font-size: 20px; line-height: 20px"
+          style="font-weight: bold; font-size: 23px; line-height: 20px"
           >SMOOTH
         </label>
         <label
           class="label2"
-          style="font-weight: bold; font-size: 20px; line-height: 20px"
+          style="font-weight: bold; font-size: 23px; line-height: 20px"
           >TRANSACTION</label
         >
-        <p class="label3">
-          <span class="name">Sell Mart</span>
+        <p class="label3" style="font-size: 18px">
+          <span class="name" style="font-size: 18px">Sell Mart</span>
           Transaction process is very easy as anyone can easily do the payment
         </p>
       </div>
       <br />
 
-      <button class="start" style="background-color: #1128a4">
+      <button class="start" style="background-color: #1128a4; margin-top: 4%; width:90%; margin-left:15px; margin-right:10px; justify-content: center">
         <a href="login.php" style="color: white">Start Shopping</a>
       </button>
     </div>
