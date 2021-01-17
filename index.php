6b2b0205009892d7283a21525fa4981dcc99e628
Charaka Andaraweera <charakasahira@gmail.com>
2021-01-18 02:09:53 +0530

    local storage creation change

--- index.php
+++ index.php
@@ -137,33 +137,6 @@
 <script>
     jQuery(document).ready(function($) {
 
-        var userInfo = {
-            "username": "Charaka",
-            "weekPoints": 22,
-            "points": 150
-        };
-
-        var pointsInfo = [{
-                "userName": "Charaka",
-                "weekPoints": 22,
-            },
-            {
-                "userName": "Navodya",
-                "weekPoints": 40,
-            },
-            {
-                "userName": "Himasha",
-                "weekPoints": 30,
-            },
-            {
-                "userName": "Uvindu",
-                "weekPoints": 25,
-            }
-        ]
-
-        //Store the object in local storage
-        localStorage.setItem('loggedUser', JSON.stringify(userInfo));
-        localStorage.setItem('pointInfo', JSON.stringify(pointsInfo));
 
         setInterval(function() {
             moveRight();
--- login.php
+++ login.php
@@ -73,6 +73,34 @@
 		<script>
 			$(window).on("load orientationchange", function() {
 
+				var userInfo = {
+            		"username": "Charaka",
+            		"weekPoints": 22,
+            		"points": 150
+        		};
+
+        		var pointsInfo = [{
+                	"userName": "Charaka",
+                	"weekPoints": 22,
+            	},
+            	{
+                	"userName": "Navodya",
+                	"weekPoints": 40,
+            	},
+            	{
+                	"userName": "Himasha",
+                	"weekPoints": 30,
+            	},
+            	{
+               		"userName": "Uvindu",
+                	"weekPoints": 25,
+            	}
+        	]
+
+        	//Store the object in local storage
+        	localStorage.setItem('loggedUser', JSON.stringify(userInfo));
+        	localStorage.setItem('pointInfo', JSON.stringify(pointsInfo));
+
 
 				if (/iPhone/i.test(navigator.userAgent)) {
 					/* checks whether it's iPhone or not*/
